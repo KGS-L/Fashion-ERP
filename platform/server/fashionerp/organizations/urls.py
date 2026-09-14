289b89b4b592e3c995d5ebda879fe5f743a327d0
SO Kevin Jonas Gningnabe <142751744+sokevinjonas@users.noreply.github.com>
2026-09-14 16:34:30 +0000

    feat: route organization API

---
+++ platform/server/fashionerp/organizations/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from .views import OrganizationDetailView, OrganizationListView
+
+
+app_name = "organizations"
+
+urlpatterns = [
+    path("", OrganizationListView.as_view(), name="list"),
+    path(
+        "<uuid:organization_id>/",
+        OrganizationDetailView.as_view(),
+        name="detail",
+    ),
+]
