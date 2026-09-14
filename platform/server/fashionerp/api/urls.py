b1fc74d1c8beaf91a9607698ac0296098eb2bb41
SO Kevin Jonas Gningnabe <142751744+sokevinjonas@users.noreply.github.com>
2026-09-14 16:41:42 +0000

    feat: expose company and establishment APIs

--- platform/server/fashionerp/api/urls.py
+++ platform/server/fashionerp/api/urls.py
@@ -6,4 +6,9 @@ app_name = "api_v1"
 urlpatterns = [
     path("auth/", include("fashionerp.identity.urls")),
     path("organizations/", include("fashionerp.organizations.urls")),
+    path("companies/", include("fashionerp.organizations.company_urls")),
+    path(
+        "establishments/",
+        include("fashionerp.organizations.establishment_urls"),
+    ),
 ]
