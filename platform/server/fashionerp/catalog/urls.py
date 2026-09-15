08391c1538bd05eb27c29fb37ea4f1be3443649f
SO Kevin Jonas Gningnabe <142751744+sokevinjonas@users.noreply.github.com>
2026-09-15 07:04:30 +0000

    feat: route fashion collection APIs

--- platform/server/fashionerp/catalog/urls.py
+++ platform/server/fashionerp/catalog/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
-from .views import ProductAttributeListCreateView, ProductDetailView, ProductListCreateView, ProductVariantCreateView
+from .views import (
+    CollectionListCreateView, FashionModelListCreateView, ProductAttributeListCreateView,
+    ProductDetailView, ProductListCreateView, ProductVariantCreateView, SeasonListCreateView,
+)
 
 app_name = "catalog"
 
 urlpatterns = [
     path("", ProductListCreateView.as_view(), name="list-create"),
     path("attributes/", ProductAttributeListCreateView.as_view(), name="attributes"),
+    path("seasons/", SeasonListCreateView.as_view(), name="seasons"),
+    path("collections/", CollectionListCreateView.as_view(), name="collections"),
+    path("fashion-models/", FashionModelListCreateView.as_view(), name="fashion-models"),
     path("<uuid:product_id>/", ProductDetailView.as_view(), name="detail"),
     path("<uuid:product_id>/variants/", ProductVariantCreateView.as_view(), name="variant-create"),
 ]
